8632981ba73c3536e40de59188d111ca6b2430c9
thegittyuser <webdevsserv@gmail.com>
2025-07-21 19:41:19 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/hero.jsx"
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+import banner from "../assets/nicky-store-banner.webp"
+import "../components/css/hero.css"
+
+function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <img src={banner} alt="" />
+      </div>
+    </>
+  );
+}
+export default Hero
