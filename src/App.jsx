0de9a25d09e67bea8f1241e594e28aca953e9ae7
thegittyuser <webdevsserv@gmail.com>
2025-07-21 20:19:25 +0500

    all updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/hero.jsx";
-import resortimage from "./assets/Write_up_DV.webp"
+import resortimage from "./assets/Write_up_DV.webp";
+import product_1 from "./assets/main-products/loose pleated shorts.webp";
+import product_2 from "./assets/main-products/textured polo shirt.webp";
+import product_3 from "./assets/main-products/textured polo shirt 2.webp";
 import "./App.css";
 
 function App() {
@@ -11,6 +14,18 @@ function App() {
       <div className="resort-image">
         <img src={resortimage} alt="" />
       </div>
+      {/* Products */}
+      <section className="main-products">
+        <div className="products-1 p-1">
+          <img src={product_1} alt="" />
+        </div>
+        <div className="products-1 p-2">
+          <img src={product_2} alt="" />
+        </div>
+        <div className="products-1 p-3">
+          <img src={product_3} alt="" />
+        </div>
+      </section>
     </>
   );
 }
