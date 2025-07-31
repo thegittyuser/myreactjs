fb4a2911a7d03f990bd65f84d93f1e5e6db79be8
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-07-31 19:24:45 +0500

    only form

--- src/hooks/Form.jsx
+++ src/hooks/Form.jsx
@@ -1,48 +1,20 @@
-import { useState } from "react";
 import "./css/form.css";
 function Form() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const formMessage = (e) => {
-    e.preventDefault();
-    alert(`All fields are required!`);
-  };
-
   return (
     <>
       <div className="form-box">
-        <form onSubmit={formMessage}>
+        <form>
           <div className="usernameInput input-box">
             <label htmlFor="username">Enter Username:</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            <input type="text" name="username" id="username" />
           </div>
           <div className="emailInput input-box">
             <label htmlFor="email">Enter Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input type="email" name="email" id="email" />
           </div>
           <div className="passwordInput input-box">
             <label htmlFor="password">Enter Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <input type="password" name="password" id="password" />
           </div>
           <div className="submitInput input-box">
             <input type="submit" />
