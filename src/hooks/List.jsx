8b92de96a77bc25bc9dbada2de23d9cf98a5b479
thegittyuser <webdevsserv@gmail.com>
2025-08-01 20:15:13 +0500

    todo list

--- src/hooks/List.jsx
+++ src/hooks/List.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import "../hooks/css/list.css";
 function List() {
+  const [input, setInput] = useState("");
+  //   const [todo, setTodo] = useState("List"); // for testing purpose
+  const [todo, setTodo] = useState("");
 
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo(input);
+    setInput("");
+  };
 
-    return (
+  return (
     <>
       <div className="list-box">
         <div className="todo-app">
-          <input type="text" name="task" id="task" placeholder="Enter Task" />
-          <input type="submit" value="Add Task"/>
+          <form className="inputbtn" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="task"
+              id="task"
+              placeholder="Enter Task"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">Add Task</button>
+          </form>
+          <div className="task-todo">
+            <li>{todo}</li>
+          </div>
         </div>
       </div>
     </>
